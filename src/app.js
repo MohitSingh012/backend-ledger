f628800afa1c6acb0111cad5cc3e743c98915ed1
MohitSingh012 <ms9936642@gmail.com>
2026-06-05 12:58:14 +0530

    Add health check route

--- src/app.js
+++ src/app.js
@@ -6,12 +6,22 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-//  Routes required
+/*
+* -  Routes required
+*/
 const authRouter = require("./routes/auth.routes");
 const accountRouter = require("./routes/account.routes");
 const transactionRoutes = require("./routes/transaction.routes");
 
-// Use Routes
+/**
+ * - Use routes
+ */
+
+app.get('/',(req,res)=>{
+    res.send("Ledger Service is up and running");
+})
+
+
 app.use("/api/auth", authRouter);
 app.use("/api/accounts", accountRouter);
 app.use("/api/transactions", transactionRoutes);
